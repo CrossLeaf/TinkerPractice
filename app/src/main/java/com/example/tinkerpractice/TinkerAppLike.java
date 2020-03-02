4d333db4549281a9ef2b845bfa490cb73dde881e
Eton <pk2p007@gmail.com>
2020-03-02 14:26:22 +0800

    🆕生成 application
    🎉 	初次 commit
    🆕 	引入新功能
    🔖 	發行/版本標籤
    🐛 	修復 bug
    🚑 	重要補丁
    🌐 	國際化与本地化
    💄 	更新 UI 和樣式文件
    🎬 	更新演示/示例
    🚨 	移除 linter 警告
    🔧 	修改配置文件
    ➕ 	增加一個依賴
    ➖ 	減少一個依賴
    ⬆️ 	升級依賴
    ⬇️ 	降級依賴
    🐎 	提升性能
    📈 	添加分析或跟踪程式碼
    🚀 	部署功能
    ✅ 	增加測試
    📝 	撰寫文檔
    🔨 	重大重構
    🎨 	改進程式碼結構/程式碼格式
    🔥 	移除程式碼或文件
    ✏️ 	修復 typo
    🚧 	工作進行中
    🗑 	廢棄或刪除
    ♿️ 	可訪問性
    👷 	添加 CI 構建系统
    💚 	修復 CI 構建問題
    🔒 	修復安全問題
    🐳 	Docker 相關工作
    🍎 	修復 macOS 下的問題
    🐧 	修復 Linux 下的問題
    🏁 	修復 Windows 下的問題

--- app/src/main/java/com/example/tinkerpractice/TinkerAppLike.java
+++ app/src/main/java/com/example/tinkerpractice/TinkerAppLike.java
@@ -1,10 +1,16 @@
 package com.example.tinkerpractice;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import androidx.multidex.MultiDex;
 
 import com.tencent.tinker.anno.DefaultLifeCycle;
 import com.tencent.tinker.entry.DefaultApplicationLike;
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 @DefaultLifeCycle(
@@ -17,5 +23,20 @@ public class TinkerAppLike extends DefaultApplicationLike {
                          Intent tinkerResultIntent) {
         super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime,
                 applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        MultiDex.install(base);
+        TinkerManager.setTinkerApplicationLike(this);
+
+        //should set before tinker is installed
+        TinkerManager.setUpgradeRetryEnable(true);
+
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        TinkerManager.installTinker(this);
+        Tinker tinker = Tinker.with(getApplication());
     }
 }
