ecf296ff531a6457b90d46e8ab32bdc2c4a06580
Eton <pk2p007@gmail.com>
2020-03-02 14:42:13 +0800

    🆕add permission
    🎉 	初次 commit
    🆕 	引入新功能
    🔖 	發行/版本標籤
    🐛 	修復 bug
    🚑 	重要補丁
    🌐 	國際化与本地化
    💄 	更新 UI 和樣式文件
    🎬 	更新演示/示例
    🚨 	移除 linter 警告
    🔧 	修改配置文件
    ➕ 	增加一個依賴
    ➖ 	減少一個依賴
    ⬆️ 	升級依賴
    ⬇️ 	降級依賴
    🐎 	提升性能
    📈 	添加分析或跟踪程式碼
    🚀 	部署功能
    ✅ 	增加測試
    📝 	撰寫文檔
    🔨 	重大重構
    🎨 	改進程式碼結構/程式碼格式
    🔥 	移除程式碼或文件
    ✏️ 	修復 typo
    🚧 	工作進行中
    🗑 	廢棄或刪除
    ♿️ 	可訪問性
    👷 	添加 CI 構建系统
    💚 	修復 CI 構建問題
    🔒 	修復安全問題
    🐳 	Docker 相關工作
    🍎 	修復 macOS 下的問題
    🐧 	修復 Linux 下的問題
    🏁 	修復 Windows 下的問題

--- app/src/main/java/com/example/tinkerpractice/MainActivity.java
+++ app/src/main/java/com/example/tinkerpractice/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.tinkerpractice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askForRequiredPermissions();
+
+        Button loadBtn = findViewById(R.id.btn_load);
+        loadBtn.setOnClickListener(view ->
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
+                        Environment.getExternalStorageDirectory().getAbsolutePath() + "/tinker_new.apk"));
+    }
+
+    private void askForRequiredPermissions() {
+        if (Build.VERSION.SDK_INT < 23) {
+            return;
+        }
+        if (!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
+    }
+
+    private boolean hasRequiredPermissions() {
+        if (Build.VERSION.SDK_INT >= 16) {
+            final int res = ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+            return res == PackageManager.PERMISSION_GRANTED;
+        } else {
+            // When SDK_INT is below 16, READ_EXTERNAL_STORAGE will also be granted if WRITE_EXTERNAL_STORAGE is granted.
+            final int res = ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            return res == PackageManager.PERMISSION_GRANTED;
+        }
     }
 }
